d95b21479f735ebb209d40e515d79c0845664575
abubakarkamar19851988@gmail.com <124461119+1kamara@users.noreply.github.com>
2023-06-26 19:47:17 +0300

    my blo-post-info

---
+++ JavacScript1/index.js
@@ -0,0 +1,123 @@
+
+// function add() {
+//     var acct=0, b=0;
+//     acct= f.dadd.value;
+//     b=acct.charAt(acct.length-1);
+//    if(b=='+' || b=='-' || b=='/' || b=='*') {
+//    f.dadd.value=acct.substring(0,acct.length-1);
+//    f.dadd.value+='+';
+//    }else {
+//  f.dadd.value+='+';
+//  } 
+//  } 
+//  function sub() {
+//     var acct=0, b=0;
+//     acct= f.dadd.value;
+//     b=acct.charAt(acct.length-1);
+//    if(b=='+' || b=='-' || b=='/' || b=='*') {
+//    f.dadd.value=acct.substring(0,acct.length-1);
+//    f.dadd.value+='-';
+//    }else {
+//  f.dadd.value+='-';
+//  } 
+//  } 
+//  function di() {
+//     var acct=0, b=0;
+//     acct= f.dadd.value;
+//     b=acct.charAt(acct.length-1);
+//    if(b=='+' || b=='-' || b=='/' || b=='*') {
+//    f.dadd.value=acct.substring(0,acct.length-1);
+//    f.dadd.value+='/';
+//    }else {
+//  f.dadd.value+='/';
+//  } 
+//  }
+//  function mul() {
+//     var acct=0, b=0;
+//     acct= f.dadd.value;
+//     b=acct.charAt(acct.length-1);
+//    if(b=='+' || b=='-' || b=='/' || b=='*') {
+//    f.dadd.value=acct.substring(0,acct.length-1);
+//    f.dadd.value+='*';
+//    }else {
+//  f.dadd.value+='*';
+//  } 
+//  } 
+
+// //  this is  EventListener ome
+//  const myEee = document.getElementById("eee");
+//  myEee.addEventListener("click", () =>{
+//     document.body.style.backgroundColor = "gold";
+//  });   
+ //  this is  EventListener 2
+//  const myCcc = document.getElementById("ccc");
+//  myEee.ondblclick = function(){
+//     document.body.style.backgroundColor = "red";
+//  };   
+
+
+// Templater String
+// const name = "Sally";
+// const age = 34;
+// const pet = "horse"
+
+// const greetingBest = `Heool ${name} you seem to be ${age-10} what a lovely ${pet} you have`;
+// console.log(greetingBest);
+
+ //Currying//
+
+//  const multiply = (a, b) => a*b;
+//  const curriedMultiply = (a) => (b) => a*b;
+//  const multiplyBy5 = curriedMultiply(5)
+//  console.log(multiplyBy5(7));
+
+//compose
+
+//object reffence//
+
+// var a =10;
+// var b = a;
+// b++;
+// const array =[1,2,3,4,5];
+// array.flat();
+// console.log(array);
+
+// const array2 =[1,[2,3],[4,5,]];
+// array2.flat();
+// console.log(array2);
+
+// const jurassicPark = [['goodbye', 'goodluck'], 'mat', 'roat' ,
+//  ['cat', 'dog'], [[[['sheep']]], 'fish'], ['meat', 'big']];
+// jurassicPark.flat(6);
+// console.log(jurassicPark);
+
+// const entery = ["Bob", "Psalm", "Peter",,,,,,,, "Momo"];
+// entery.flat();
+// console.log(entery);
+
+// const jurassicParkChaos = jurassicPark.flatMap(creature => creature + 'dog');
+// console.log(jurassicParkChaos);
+
+// const userEmail1 = '        kamarafofeabu@gmail.com';
+// const userEmail2 = 'omuarufofeekamar@gmail.com       ';
+// console.log(userEmail1.trimStart());
+// console.log(userEmail2.trimEnd());
+// console.clear();
+// var firstName = "Fofee Abubakar";
+// var lastName = "Kamara";
+// console.log("firstName:", firstName, "and lastName:",
+//  lastName);
+//  var foo = "Kamara";
+//  console.log(foo);
+//  var greet = "Hello", who = "Fofee";
+//  console.log("%s, %s", greet, who);
+//  document.getElementById("my-java").textContent = 
+//  "This is Mr Fofee Kamara webpge"
+//  var elem = document.createElement("p");
+//  elem.textContent = "Omaru A Kamara is the elder brother of Mr Fofee and Famata A Kamara is their elder sister";
+//  document.body.appendChild(elem);
+// var myStr = "I am a" +" string";
+
+
+
+
